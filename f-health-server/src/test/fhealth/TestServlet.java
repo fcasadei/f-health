161ae3916e99c156545cf934db6bdd8b67fcd101
Flavio <fcasadei@gmail.com>
2017-03-22 21:49:15 +0100

    inizio progetto

---
+++ f-health-server/src/test/fhealth/TestServlet.java
@@ -0,0 +1,45 @@
+package test.fhealth;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class TestServlet
+ */
+public class TestServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public TestServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String s = getInitParameter("testo");
+		PrintWriter wr = null;
+		try {
+			wr =response.getWriter();
+			wr.println(s);
+		} finally {
+			/*if (wr != null) {
+				wr.close();
+			}*/
+		}
+		
+		
+	}
+
+	
+
+}
